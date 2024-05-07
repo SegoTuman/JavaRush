2109e3ae6f8db27f1cafc4dbe7ae6514076d613d
Sergey <118913401+SegoTuman@users.noreply.github.com>
2024-05-07 15:41:18 +0300

    .

--- AnimalsOfMight&Magic/src/enums/DirectionType.java
+++
@@ -1,9 +0,0 @@
-package enums;
-
-public enum DirectionType {
-    LEFT,
-    RIGHT,
-    UP,
-    DOWN
-
-}
--- AnimalsOfMight&Magic/src/models/animals/herbivores/Herbivore.java
+++
@@ -1,9 +0,0 @@
-package models.animals.herbivores;
-
-import abstracts.Animal;
-
-public abstract class Herbivore extends Animal {
-    public Herbivore(double weight, int speed, int maxPopulation, Double kgToFullEating) {
-        super(weight, speed, maxPopulation, kgToFullEating);
-    }
-}
